dee42efb0ef802d5965cb59d8d56bdb5db04d46d
rahin21 <mdrahinzaman21@gmail.com>
2024-06-21 21:15:52 +0600

    fixed login issue

--- app/(backend)/login/page.tsx
+++ app/(backend)/login/page.tsx
@@ -1,28 +1,49 @@
 import React from "react";
 import type { Metadata } from "next";
-
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Box,
+  Flex,
+  Heading,
+} from "@chakra-ui/react";
 import LoginForm from "@/components/loginForm";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { Container } from "@chakra-ui/react";
+
 export const metadata: Metadata = {
   title: "Login",
 };
 
-
 async function page() {
   const session = await getServerSession(authOptions);
-  if(!session?.user){
-  return (
-    <div className="container flex flex-col justify-center mx-auto lg:px-0 px-5 h-screen">
-      <div className="flex flex-col justify-content-center text-justify [text-align-last:center] font-semibold tracking-[2px] leading-7 text-[16px] text-lightText">
-          <LoginForm/>
-      </div>
-    </div>
-  );
-} else {
-  redirect('/admin')
-}
+  if (!session?.user) {
+    return (
+      <Flex
+        minHeight="100vh"
+        minWidth="max-content"
+        alignItems="center"
+        justify={"center"}
+      >
+        <Container>
+          <Card boxShadow="xl" variant={'outline'}>
+            <CardHeader >
+              <Heading size="md" textAlign={"center"}>Login</Heading>
+            </CardHeader>
+            <CardBody>
+              <LoginForm />
+            </CardBody>
+          </Card>
+        </Container>
+      </Flex>
+    );
+  } else {
+    redirect("/admin");
+  }
 }
 
 export default page;
--- components/loginForm.tsx
+++ components/loginForm.tsx
@@ -9,36 +9,37 @@ import {
   FormErrorMessage,
   FormHelperText,
   Button,
-} from '@chakra-ui/react';
+  Center,
+} from "@chakra-ui/react";
 
-import { Input } from '@chakra-ui/react'
-
+import { Input } from "@chakra-ui/react";
 
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   email: z.string().email("Invalid Email").min(5, "Email is too short"),
-  password: z.string().min(8, "Password is too short"),
+  password: z.string().min(6, "Password is too short"),
 });
 type formSchema = z.infer<typeof formSchema>;
 
 function LoginForm() {
-
   const router = useRouter();
-  
+
   const {
     handleSubmit,
     register,
     formState: { errors, isSubmitting },
   } = useForm<formSchema>({
-    resolver: zodResolver(formSchema)});
+    resolver: zodResolver(formSchema),
+  });
 
   // 2. Define a submit handler.
-  const onSubmit:SubmitHandler<formSchema> = async (values: formSchema) => {
+  const onSubmit: SubmitHandler<formSchema> = async (values: formSchema) => {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values);
+    // console.log(values);
+    console.log("Submit button clicked");
     const loginData = await signIn("credentials", {
       email: values.email,
       password: values.password,
@@ -49,44 +50,46 @@ function LoginForm() {
     } else {
       router.push("/admin");
     }
-  }
+  };
 
   return (
-  
     <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={errors.email}>
+        <FormLabel htmlFor="email" className="">
+          Email:
+        </FormLabel>
 
-            
-            <FormControl isInvalid={errors.email}>
-                <FormLabel htmlFor="email" className="">Email:</FormLabel>
-                
-                  <Input
-                    placeholder="Email"
-                    type="email"
-                    className="text-start placeholder:text-start"
-                    {...register("email")}
-                  />
-              
-              <FormErrorMessage>
-              {errors.email && errors.email.message}
-              </FormErrorMessage>
-              </FormControl>
-         
+        <Input
+          placeholder="Email"
+          type="email"
+          className="text-start placeholder:text-start"
+          {...register("email")}
+        />
 
-            <FormControl isInvalid={errors.email}>
-                <FormLabel htmlFor="password" className="">Password:</FormLabel>
-                <Input
-                  placeholder="Password"
-                  type="password"
-                  className="text-start placeholder:text-start"
-                  {...register("password")}
-                />
-                
-              <FormErrorMessage >
-                {errors.password && errors.password.message}
-              </FormErrorMessage>
-              </FormControl>
+        <FormErrorMessage>
+          {errors.email && errors.email.message}
+        </FormErrorMessage>
+      </FormControl>
 
+      <FormControl isInvalid={errors.email}>
+        <FormLabel htmlFor="password" className="">
+          Password:
+        </FormLabel>
+        <Input
+          placeholder="Password"
+          type="password"
+          className="text-start placeholder:text-start"
+          {...register("password")}
+        />
+
+        <FormErrorMessage>
+          {errors.password && errors.password.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <Center paddingTop={"20px"}>
         <Button type="submit">Submit</Button>
+      </Center>
     </form>
   );
 }
