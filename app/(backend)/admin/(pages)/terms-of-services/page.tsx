1deee9b876903e823d01055613a08028e71be7b2
rahin21 <mdrahinzaman21@gmail.com>
2024-07-25 18:37:49 +0600

    terms

--- app/(backend)/admin/(pages)/terms-of-services/page.tsx
+++ app/(backend)/admin/(pages)/terms-of-services/page.tsx
@@ -1,11 +1,34 @@
 import PrivacyTermForm from '@/components/tailAdmin/privacy/privacyForm'
 import PrivacyTable from '@/components/tailAdmin/privacy/privacyTable'
-import { getTermData } from '@/constants/admin/privacyData'
+// import { getTermData } from '@/constants/admin/privacyData'
 import React from 'react'
+
+export async function getTermData() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXTAUTH_URL}/api/privacy-term/terms`,
+      {
+        next: { tags: ["privacyTerms"] },
+      }
+    );
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+}
 
 async function TermsOfServices() {
   const term = await getTermData();
-  console.log(term);
+
   return (
     <div>
        <h4 className="text-2xl font-semibold text-black mb-4">
