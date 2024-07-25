93446fea5571e9296ca1651503347e59df33775b
rahin21 <mdrahinzaman21@gmail.com>
2024-07-25 18:16:44 +0600

    fixes

--- constants/admin/privacyData.ts
+++ constants/admin/privacyData.ts
@@ -1,4 +1,6 @@
 export async function getPrivacyData() {
+  try {
+    
     const res = await fetch(`${process.env.NEXTAUTH_URL}/api/privacy-term/privacy`, {
       next: { tags: ["privacyTerms"] },
     });
@@ -11,20 +13,30 @@ export async function getPrivacyData() {
     }
   
     return res.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null; 
+  }
   }
   
   export async function getTermData() {
-    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/privacy-term/terms`, {
-      next: { tags: ["privacyTerms"] },
-    });
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-  
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error("Failed to fetch data");
+    try {
+      
+      const res = await fetch(`${process.env.NEXTAUTH_URL}/api/privacy-term/terms`, {
+        next: { tags: ["privacyTerms"] },
+      });
+      // The return value is *not* serialized
+      // You can return Date, Map, Set, etc.
+    
+      if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error("Failed to fetch data");
+      }
+    
+      return res.json();
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    return null; 
     }
-  
-    return res.json();
   }
   
