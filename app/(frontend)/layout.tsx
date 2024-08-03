b4dda288a64bf9596c17f9012b0773107b860e9d
rahin21 <mdrahinzaman21@gmail.com>
2024-08-03 12:39:09 +0600

    fix open graph

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,41 +1,47 @@
 import type { Metadata } from "next";
-import localfont from "next/font/local"
-import image from "/public/opengraph-image.png"
+import localfont from "next/font/local";
+import image from "/public/og-image.png";
 import "../globals.css";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
 import ScrollToTop from "@/components/scrollToTop";
 
 const dipotic = localfont({
-  src: [{
-    path: "../../assets/fonts/dipotic/Didot-Italic.otf",
-  }],
-  variable:"--font-dipotic"
-})
+  src: [
+    {
+      path: "../../assets/fonts/dipotic/Didot-Italic.otf",
+    },
+  ],
+  variable: "--font-dipotic",
+});
 const openSans = localfont({
-  src: [{
-    path: "../../assets/fonts/open-sans/OpenSans-VariableFont_wdth,wght.ttf",
-  }],
-  variable:"--font-openSans"
-})
+  src: [
+    {
+      path: "../../assets/fonts/open-sans/OpenSans-VariableFont_wdth,wght.ttf",
+    },
+  ],
+  variable: "--font-openSans",
+});
 const palatino = localfont({
-  src: [{
-    path: "../../assets/fonts/palatino/Palatino LT Bold.ttf",
-  }],
-  variable:"--font-palatino"
-})
+  src: [
+    {
+      path: "../../assets/fonts/palatino/Palatino LT Bold.ttf",
+    },
+  ],
+  variable: "--font-palatino",
+});
 
 export const metadata: Metadata = {
   metadataBase: new URL(`https://ambiance-sigma.vercel.app`),
   title: {
-    default:"Home - Ambiance",
-    template:"%s - Ambiance",
+    default: "Home - Ambiance",
+    template: "%s - Ambiance",
   },
   description: "The Interior Design",
-  openGraph:{
-    title: "Ambiance",
-    images:[{url:image.src}]
-  }
+  openGraph: {
+    title: "Ambiance Shit",
+    images: [{ url: image.src }],
+  },
 };
 
 export default function RootLayout({
@@ -45,12 +51,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${dipotic.variable} ${openSans.variable} ${palatino.variable}`}>
-        <Nav/>
+      <body
+        className={`${dipotic.variable} ${openSans.variable} ${palatino.variable}`}
+      >
+        <Nav />
         {children}
-        <ScrollToTop/>
-        <Footer/>
-        </body>
+        <ScrollToTop />
+        <Footer />
+      </body>
     </html>
   );
 }
